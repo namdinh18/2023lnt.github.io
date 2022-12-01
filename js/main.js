d58c8e4ed1d6260ae794b4e25fa198c63f98d988
namdinh18 <119444588+namdinh18@users.noreply.github.com>
2022-12-02 00:17:09 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,217 +1,357 @@
-﻿document.addEventListener('DOMContentLoaded', function() {
-  let targetDate = new Date(
-    "'02/01/2022 00:00:00 GMT+0700 (SE Asia Standard Time)"
-  );
-  let onStart = () => {
-    document
-      .querySelectorAll('.countdown-item')
-      .forEach((item) => item.classList.add('show'));
-  };
-  let onTick = ({ days, hours, minutes, seconds }) => {
-    document.querySelector('.countdown-item.days').innerHTML = days;
-    document.querySelector('.countdown-item.hours').innerHTML = hours;
-    document.querySelector('.countdown-item.minutes').innerHTML = minutes;
-    document.querySelector('.countdown-item.seconds').innerHTML = seconds;
-  };
-  let options = new LsCountdownOptions({ targetDate, onStart, onTick });
-  let countdown = new LsCountdown(options);
+/*****************************************************************************
+  _____  _       _     _____  _                
+ |  __ \(_)     | |   |  __ \| |               
+ | |  | |_ _ __ | |__ | |__) | |__  _   _  ___ 
+ | |  | | | '_ \| '_ \|  ___/| '_ \| | | |/ __|
+ | |__| | | | | | | | | |    | | | | |_| | (__ 
+ |_____/|_|_| |_|_| |_|_|    |_| |_|\__,_|\___|
+                                               
+                                               
+******************************************************************************/
 
-  countdown.start();
-  window.mycountdown = countdown;
+/*======== Window Load Function ========*/
+$(window).on('load', function () {
+
+    /*======== Preloader ========*/
+    $(".loader").fadeOut();
+    $(".preloader").delay(100).fadeOut();
+    $("#old").text(new Date().getFullYear() - 1997);
+    $("#experience").text(new Date().getFullYear() - 2018);
+
+    /*======== Isotope Portfolio Setup ========*/
+    if ($('.portfolio-items').length) {
+        var $elements = $(".portfolio-items"),
+            $filters = $('.portfolio-filter ul li');
+        $elements.isotope();
+
+        $filters.on('click', function () {
+            $filters.removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).data('filter');
+            $(".portfolio-items").isotope({
+                filter: selector,
+                hiddenStyle: {
+                    transform: 'scale(.2) skew(30deg)',
+                    opacity: 0
+                },
+                visibleStyle: {
+                    transform: 'scale(1) skew(0deg)',
+                    opacity: 1,
+                },
+                transitionDuration: '.5s'
+            });
+        });
+    }
+
+    /*======== Blogs Masonry Setup ========*/
+    $('.blogs-masonry').isotope({ layoutMode: 'moduloColumns' });
+
 });
-!(function(t) {
-  var e = {};
-  function s(n) {
-    if (e[n]) return e[n].exports;
-    var o = (e[n] = { i: n, l: !1, exports: {} });
-    return t[n].call(o.exports, o, o.exports, s), (o.l = !0), o.exports;
-  }
-  (s.m = t),
-    (s.c = e),
-    (s.d = function(t, e, n) {
-      s.o(t, e) || Object.defineProperty(t, e, { enumerable: !0, get: n });
-    }),
-    (s.r = function(t) {
-      'undefined' != typeof Symbol &&
-        Symbol.toStringTag &&
-        Object.defineProperty(t, Symbol.toStringTag, { value: 'Module' }),
-        Object.defineProperty(t, '__esModule', { value: !0 });
-    }),
-    (s.t = function(t, e) {
-      if ((1 & e && (t = s(t)), 8 & e)) return t;
-      if (4 & e && 'object' == typeof t && t && t.__esModule) return t;
-      var n = Object.create(null);
-      if (
-        (s.r(n),
-        Object.defineProperty(n, 'default', { enumerable: !0, value: t }),
-        2 & e && 'string' != typeof t)
-      )
-        for (var o in t)
-          s.d(
-            n,
-            o,
-            function(e) {
-              return t[e];
-            }.bind(null, o)
-          );
-      return n;
-    }),
-    (s.n = function(t) {
-      var e =
-        t && t.__esModule
-          ? function() {
-              return t.default;
+
+
+/*======== Document Ready Function ========*/
+$(document).ready(function () {
+
+    "use strict";
+
+
+    /*======== SimpleBar Setup ========*/
+    $('.pt-page').each(function () {
+        var $id = '#' + $(this).attr('id');
+        new SimpleBar($($id)[0]);
+    });
+
+    /*======== Fitty Setup ========*/
+    fitty('.header-name', {
+        multiLine: false,
+        maxSize: 20,
+        minSize: 10
+    });
+
+    /*======== Active Current Link ========*/
+    $('.nav-menu a').on('click', function () {
+        if ($('.header-content.on').length) {
+            $('.header-content').removeClass('on');
+        }
+    });
+
+    /*======== Mobile Toggle Click Setup ========*/
+    $('.header-toggle').on('click', function () {
+        $('header .header-content').toggleClass('on');
+    });
+
+    /*========Clients OwlCarousel Setup========*/
+    $(".clients .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        smartSpeed: 500,
+        responsiveClass: true,
+        autoplayHoverPause: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            500: {
+                items: 3,
+            },
+            700: {
+                items: 4,
+            },
+            1000: {
+                items: 6,
+            },
+        },
+    });
+
+    /*========Testimonials OwlCarousel Setup========*/
+    $(".testimonials .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        smartSpeed: 500,
+        responsiveClass: true,
+        dots: false,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            800: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            },
+        },
+    });
+
+    /*======== Skills Progress Animation ========*/
+    if ($('.skills').length > 0) {
+        var el = new SimpleBar($('#resume')[0]).getScrollElement();
+
+        $(el).on('scroll', function () {
+
+            $('.progress .progress-bar').each(function () {
+                var bottom_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_window = $(window).scrollTop() + $(window).height();
+                var progressWidth = $(this).data('progress-value') + '%';
+                if (bottom_window > bottom_object) {
+                    $(this).css({
+                        width: progressWidth
+                    });
+                    $(this).find('.progress-value').animate({
+                        countNum: parseInt(progressWidth, 10)
+                    }, {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function () {
+                            $(this).text(Math.floor(this.countNum) + '%');
+                        },
+                        complete: function () {
+                            $(this).text(this.countNum + '%');
+                        }
+                    });
+                }
+            });
+
+        });
+    }
+
+    /*======== Portfolio Image Link Setup ========*/
+    $('.portfolio-items .image-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    /*======== Portfolio Video Link Setup ========*/
+    $('.portfolio-items .video-link').magnificPopup({
+        type: 'iframe',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    /*======== Portfolio Ajax Link Setup ========*/
+    ajaxPortfolioSetup($('.portfolio-items .ajax-link'), $('.ajax-portfolio-popup'));
+
+    /*======== Portfolio Tilt Setup ========*/
+    $('#portfolio .item figure').tilt({
+        maxTilt: 3,
+        glare: true,
+        maxGlare: .6,
+        reverse: true
+    });
+
+    /*======== Google Map Setup ========*/
+    if ($('#map').length) {
+        initMap();
+    }
+
+
+    /*======== Contact Form Setup ========*/
+    contactFormSetup();
+
+    /*======== Change backgorund ========*/
+    changeBackground()
+});
+
+
+/*********** Function Ajax Portfolio Setup **********/
+function ajaxPortfolioSetup($ajaxLink, $ajaxContainer) {
+    $ajaxLink.on('click', function (e) {
+        var link = $(this).attr('href');
+
+        if (link === "#") {
+            e.preventDefault();
+            return;
+        }
+
+        $ajaxContainer.find('.content-wrap .popup-content').empty();
+
+        $ajaxContainer.addClass('on');
+        $.ajax({
+            url: link,
+            beforeSend: function () {
+                $ajaxContainer.find('.ajax-loader').show();
+            },
+            success: function (result) {
+                $ajaxContainer.find('.content-wrap .popup-content').html(result);
+            },
+            complete: function () {
+                $ajaxContainer.find('.ajax-loader').hide();
+            },
+            error: function (e) {
+                $ajaxContainer.find('.ajax-loader').hide();
+                $ajaxContainer.find('.content-wrap .popup-content').html('<h1 class="text-center">Something went wrong! Retry or refresh the page.</h1>')
             }
-          : function() {
-              return t;
-            };
-      return s.d(e, 'a', e), e;
-    }),
-    (s.o = function(t, e) {
-      return Object.prototype.hasOwnProperty.call(t, e);
-    }),
-    (s.p = ''),
-    s((s.s = 2));
-})([
-  function(t, e, s) {
-    'use strict';
-    Object.defineProperty(e, '__esModule', { value: !0 });
-    e.default = class {
-      constructor({
-        days: t = '0',
-        hours: e = '0',
-        minutes: s = '0',
-        seconds: n = '0'
-      }) {
-        (this.days = t),
-          (this.hours = e),
-          (this.minutes = s),
-          (this.seconds = n);
-      }
+        });
+        e.preventDefault();
+    });
+
+    $ajaxContainer.find('.popup-close').on('click', function () {
+        $ajaxContainer.removeClass('on');
+    });
+
+
+}
+
+
+/********** Function Map Initialization **********/
+function initMap() {
+    var latitude = $("#map").data('latitude'),
+        longitude = $("#map").data('longitude'),
+        zoom = $("#map").data('zoom'),
+        cordinates = new google.maps.LatLng(latitude, longitude);
+
+    var styles = [{ "stylers": [{ "saturation": -100 }, { "gamma": 0.8 }, { "lightness": 4 }, { "visibility": "on" }] }, { "featureType": "landscape.natural", "stylers": [{ "visibility": "on" }, { "color": "#5dff00" }, { "gamma": 4.97 }, { "lightness": -5 }, { "saturation": 100 }] }];
+    var mapOptions = {
+        zoom: zoom,
+        center: cordinates,
+        mapTypeControl: false,
+        disableDefaultUI: true,
+        zoomControl: true,
+        scrollwheel: false,
+        styles: styles
     };
-  },
-  function(t, e, s) {
-    'use strict';
-    Object.defineProperty(e, '__esModule', { value: !0 });
-    e.default = class {
-      constructor({
-        days: t = 'd',
-        hours: e = 'h',
-        minutes: s = 'm',
-        seconds: n = 's'
-      }) {
-        (this.days = t),
-          (this.hours = e),
-          (this.minutes = s),
-          (this.seconds = n);
-      }
-    };
-  },
-  function(t, e, s) {
-    'use strict';
-    Object.defineProperty(e, '__esModule', { value: !0 });
-    const n = s(3);
-    e.LsCountdown = n.default;
-    const o = s(4);
-    e.LsCountdownOptions = o.default;
-    const i = s(1);
-    e.LsCountdownSufixes = i.default;
-    const r = s(0);
-    (e.LsCountdownTick = r.default),
-      ((t) => {
-        (t.LsCountdown = n.default),
-          (t.LsCountdownOptions = o.default),
-          (t.LsCountdownSufixes = i.default),
-          (t.LsCountdownTick = r.default);
-      })('undefined' != typeof window ? window : {});
-  },
-  function(t, e, s) {
-    'use strict';
-    Object.defineProperty(e, '__esModule', { value: !0 });
-    const n = s(0);
-    e.default = class {
-      constructor(t) {
-        this.validateTargetDate(t.targetDate),
-          (this.targetDate = t.targetDate),
-          (this.onStart = t.onStart),
-          (this.onStop = t.onStop),
-          (this.onTick = t.onTick),
-          (this.sufixes = t.sufixes),
-          (this.CURRENT_TIME = new n.default({}));
-      }
-      validateTargetDate(t) {
-        if (t <= new Date())
-          throw new Error('The target date must be a foward date');
-      }
-      doTick(t) {
-        let e,
-          s,
-          o,
-          i,
-          r = new Date().getTime(),
-          u = (this.targetDate.getTime() - r) / 1e3;
-        (e = u / 86400),
-          (s = (u %= 86400) / 3600),
-          (o = (u %= 3600) / 60),
-          (i = u % 60),
-          (e =
-            parseInt(e > 0 ? (e > 9 ? e : `0${e}`) : '00').toString() +
-            this.sufixes.days),
-          (s =
-            parseInt(s > 0 ? (s > 9 ? s : `0${s}`) : '00').toString() +
-            this.sufixes.hours),
-          (o =
-            parseInt(o > 0 ? (o > 9 ? o : `0${o}`) : '00').toString() +
-            this.sufixes.minutes),
-          (i =
-            parseInt(i > 0 ? (i > 9 ? i : `0${i}`) : '00').toString() +
-            this.sufixes.seconds),
-          (this.CURRENT_TIME = new n.default({
-            days: e,
-            hours: s,
-            minutes: o,
-            seconds: i
-          })),
-          'function' == typeof t && t(this.CURRENT_TIME);
-      }
-      stop() {
-        this.COUNTDOWN_INTERVAL &&
-          (clearInterval(this.COUNTDOWN_INTERVAL),
-          'function' == typeof this.onStop &&
-            this.onStop(Object.assign({}, this.CURRENT_TIME)));
-      }
-      start() {
-        this.doTick(this.onStart),
-          (this.COUNTDOWN_INTERVAL = setInterval(() => {
-            this.doTick(this.onTick);
-          }, 1e3));
-      }
-      changeTargetDate(t = new Date()) {
-        this.validateTargetDate(t),
-          this.stop.bind(this).call(),
-          (this.targetDate = t),
-          this.start.bind(this).call();
-      }
-    };
-  },
-  function(t, e, s) {
-    'use strict';
-    Object.defineProperty(e, '__esModule', { value: !0 });
-    const n = s(1);
-    e.default = class {
-      constructor({
-        targetDate: t = new Date(),
-        onStart: e = () => {},
-        onStop: s = () => {},
-        onTick: o = () => {},
-        sufixes: i = new n.default({})
-      } = {}) {
-        (this.targetDate = t),
-          (this.onStart = e),
-          (this.onStop = s),
-          (this.onTick = o),
-          (this.sufixes = i);
-      }
-    };
-  }
-]);
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: cordinates,
+        map: map,
+        title: "We are here!"
+    });
+}
+
+/********** Function Contact Form Setup **********/
+function contactFormSetup() {
+
+    /*======== Check Field Have Value When Page Load ========*/
+    $('.input__field').each(function () {
+        if ($(this).val()) {
+            $(this).parent('.input').addClass('input--filled');
+        } else {
+            $(this).parent('.input').removeClass('input--filled');
+        }
+    });
+
+    /*======== Check Field Have Value When Keyup ========*/
+    $('.input__field').on('keyup', function () {
+        if ($(this).val()) {
+            $(this).parent('.input').addClass('input--filled');
+        } else {
+            $(this).parent('.input').removeClass('input--filled');
+        }
+    });
+
+
+    $('#contact-form').on('submit', function (e) {
+        e.preventDefault();
+        var name = $('#cf-name').val(),
+            email = $('#cf-email').val(),
+            message = $('#cf-message').val(),
+            $messageBox = $('#contact-form .message'),
+            required = 0;
+
+
+        $('.cf-validate', this).each(function () {
+            if ($(this).val() == '') {
+                $(this).addClass('cf-error');
+                required += 1;
+            } else {
+                if ($(this).hasClass('cf-error')) {
+                    $(this).removeClass('cf-error');
+                    if (required > 0) {
+                        required -= 1;
+                    }
+                }
+            }
+        });
+        if (required === 0) {
+            $.ajax({
+                type: 'POST',
+                url: 'mail.php',
+                data: {
+                    cf_name: name,
+                    cf_email: email,
+                    cf_message: message
+                },
+                success: function (data) {
+                    $("#contact-form .input__field").val("");
+                    showAlertBox(data.status, data.responseText);
+                },
+                error: function (data) {
+                    showAlertBox(data.status, data.responseText);
+                }
+            });
+        }
+    });
+}
+
+
+/********** Function Show Alert Box **********/
+function showAlertBox(response, message) {
+    var $alertBox = $('<div class="alert"></div>'),
+        $alContainer = $('#contact-form .alert-container');
+    if (response == 200) {
+        $alertBox.addClass('alert-success').html(message);
+        $alContainer.html($alertBox);
+    } else {
+        $alertBox.addClass('alert-danger').html(message);
+        $alContainer.html($alertBox);
+    }
+    $alContainer.fadeIn(300).delay(2000).fadeOut(400);
+}
+
+function changeBackground() {
+    var topic = "code";
+    var url = "https://source.unsplash.com/1600x900/?" + topic;
+ 
+    var arrImg = [];
+    for (let index = 0; index < 30; index++) {
+        arrImg.push(url + '-' + index);
+    }
+    $("#home").backstretch(arrImg, { duration: 5000, fade: 1000 });
+}
